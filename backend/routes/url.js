ebf0a21cf9fd0a6048141aece0ceb0d4dc692126
payaljindal <payaljindal05052001@gmail.com>
2020-06-25 14:51:22 +0530

    added api to shorten links

--- backend/routes/url.js
+++ backend/routes/url.js
@@ -5,6 +5,7 @@ const shortid = require('shortid');
 const config = require('config');
 const Url = require('../models/url-model');
 const HttpError = require('../models/http-error');
+const fetch = require('node-fetch');
 
 // route to shorten the link
 router.post('/shorten', async (req, res) => {
@@ -12,48 +13,21 @@ router.post('/shorten', async (req, res) => {
   const { keyword } = req.body;
   const baseUrl = config.get('baseUrl');
 
-  //check base url
-  if (!validUrl.isUri(baseUrl)) {
-    return res.status(401).json('Invalid base url');
-  }
-  // Create url code
-  const urlCode = shortid.generate();
-
-  // Check Long Url is valid or not
-  if (validUrl.isUri(longUrl)) {
-    try {
-      let url = await Url.findOne({ longUrl, keyword });
-
-      // if longurl is shortened already, then it will return same shorturl
-      if (url) {
-        res.json(url);
-      } else {
-        const shortUrl = baseUrl + '/' + urlCode;
-        url = new Url({
-          keyword,
-          longUrl,
-          shortUrl,
-          urlCode,
-          date: new Date(),
-        });
-        await url.save();
-
-        res.json(url);
-      }
-    } catch (err) {
-      const error = new HttpError(
-        'Internal Server Error. Please try again later!',
-        500,
-      );
-      return next(error);
-    }
-  } else {
-    if (longUrl == '') {
-      res.status(401).json('URL can not be empty!');
-    } else {
-      res.status(401).json('Invalid URL!');
-    }
-  }
+  const fetch_response = await fetch(
+    'https://kutt.it/api/v2/links?apikey=***',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        target: longUrl,
+        domain: 'dsctiet.tech',
+      }),
+    },
+  );
+  const response_json = await fetch_response.json();
+  res.send(response_json);
 });
 
 module.exports = router;
--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -23,7 +23,7 @@ const keywords = [
   },
   {
     value: '',
-    label: 'lorem ipsum',
+    label: 'other',
   },
 ];
 class HomeScreen extends Component {
@@ -86,16 +86,17 @@ class HomeScreen extends Component {
       });
 
       const responseData = await response.json();
-      if (typeof responseData == 'object') {
+
+      if (typeof responseData.error !== 'undefined') {
         this.setState({
-          shortUrl: responseData.shortUrl,
-          submitButton: true,
+          error: responseData.error,
+          submitButton: false,
         });
-        console.log(responseData.keyword);
-      } else {
-        console.log(responseData);
+      }
+      if (typeof responseData.link !== 'undefined') {
         this.setState({
-          error: responseData,
+          shortUrl: responseData.link,
+          submitButton: true,
         });
       }
     } catch (err) {
