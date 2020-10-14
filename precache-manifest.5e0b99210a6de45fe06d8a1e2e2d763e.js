19788ed47ccce8d3c41ece5e164bfb0b4a31d7c3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-14 11:14:47 +0000

    Deploy developer-student-club-thapar/urlShortner to github.com/developer-student-club-thapar/urlShortner.git:gh-pages

---
+++ precache-manifest.5e0b99210a6de45fe06d8a1e2e2d763e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c65245d2084794c2b489d48efe4e494",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3538924354bcb1a048ba",
+    "url": "/static/css/main.edd0c83e.chunk.css"
+  },
+  {
+    "revision": "9ef911fbd554c70e787a",
+    "url": "/static/js/2.9e5cc177.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.9e5cc177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3538924354bcb1a048ba",
+    "url": "/static/js/main.9a628ed8.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  },
+  {
+    "revision": "0d517ed634ef717d1e40123b3af77c11",
+    "url": "/static/media/dsc_logo.0d517ed6.png"
+  }
+]);
